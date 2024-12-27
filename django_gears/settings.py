660cf0a56bb039519bde173fd3cd483f83db91d1
Jakub Dvorak <nytmyn@gmail.com>
2024-12-27 20:53:14 +0100

    remove extra list conversions

--- django_gears/settings.py
+++ django_gears/settings.py
@@ -59,10 +59,10 @@ for path in getattr(settings, 'GEARS_FINDERS', DEFAULT_FINDERS):
     environment.finders.register(get_finder(path, options))
 
 mimetypes = getattr(settings, 'GEARS_MIMETYPES', DEFAULT_MIMETYPES)
-for extension, mimetype in list(mimetypes.items()):
+for extension, mimetype in mimetypes.items():
     environment.mimetypes.register(extension, mimetype)
 
-for extension, path in list(getattr(settings, 'GEARS_COMPILERS', {}).items()):
+for extension, path in getattr(settings, 'GEARS_COMPILERS', {}).items():
     if isinstance(path, (list, tuple)):
         path, options = path
     else:
@@ -70,19 +70,19 @@ for extension, path in list(getattr(settings, 'GEARS_COMPILERS', {}).items()):
     environment.compilers.register(extension, get_asset_handler(path, options))
 
 preprocessors = getattr(settings, 'GEARS_PREPROCESSORS', DEFAULT_PREPROCESSORS)
-for mimetype, paths in list(preprocessors.items()):
+for mimetype, paths in preprocessors.items():
     if not isinstance(paths, (list, tuple)):
         paths = [paths]
     for path in paths:
         environment.preprocessors.register(mimetype, get_asset_handler(path))
 
 postprocessors = getattr(settings, 'GEARS_POSTPROCESSORS', DEFAULT_POSTPROCESSORS)
-for mimetype, paths in list(postprocessors.items()):
+for mimetype, paths in postprocessors.items():
     if not isinstance(paths, (list, tuple)):
         paths = [paths]
     for path in paths:
         environment.postprocessors.register(mimetype, get_asset_handler(path))
 
 compressors = getattr(settings, 'GEARS_COMPRESSORS', {})
-for mimetype, path in list(compressors.items()):
+for mimetype, path in compressors.items():
     environment.compressors.register(mimetype, get_asset_handler(path))
